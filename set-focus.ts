dbbadb48c9573d4c7bde23ae97b99da4f2bec671
Gary Taylor <gravity.addiction@gmail.com>
2016-05-09 11:40:09 -0700

    init

---
+++ set-focus.ts
@@ -0,0 +1,20 @@
+import {Directive, ElementRef} from '@angular/core';
+
+@Directive({
+  selector: '[setFocus]'
+})
+export class SetFocusDirective {
+
+  constructor(private el: ElementRef) {
+    setTimeout(function() {
+      let end = el.nativeElement.value.length;
+
+      if (typeof el.nativeElement.selectionStart !== 'undefined') {
+        el.nativeElement.selectionStart = end;
+        el.nativeElement.selectionEnd = end;
+      }
+    }, 0);
+  }
+
+}
+
